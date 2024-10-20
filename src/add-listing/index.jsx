03ffe8b1f932e45ee7500b5b815e06e25ae4a4a3
vishal <141538917+vkprajapati96@users.noreply.github.com>
2024-10-20 16:50:35 +0530

    Update index.jsx

--- src/add-listing/index.jsx
+++ src/add-listing/index.jsx
@@ -1,6 +1,6 @@
 import Header from '@/components/Header'
 import carDetails from "../Shared/carDetails.json"
-import InputField from './components/inputField'
+import InputField from './components/InputField'
 import DropdownField from './components/DropdownField'
 import TextAreaField from './components/TextAreaField'
 import features from '../Shared/features.json'
